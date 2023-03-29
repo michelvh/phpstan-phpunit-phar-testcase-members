23daaede919549043fead48e0247d6bc90b6c4eb
Michel van Heijster <michel@timetocode.nl>
2023-03-29 15:08:07 +0200

    Initial commit

---
+++ src/Foo.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace TimeToCode;
+
+class Foo
+{
+
+}
---
+++ tests/FooTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace TimeToCode\Tests;
+
+use PHPUnit\Framework\TestCase;
+use TimeToCode\Foo;
+
+class FooTest extends TestCase
+{
+    public Foo $foo;
+}
